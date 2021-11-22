f1f31f72eddcdeb7448eb68c13f10a4106c77abe
Egor Demidyuk <demidrom@mail.ru>
2021-11-22 16:13:10 +0300

    White IP

--- rmq_consumer.sh
+++ rmq_consumer.sh
@@ -1,3 +1,3 @@
-python3 ./code/rmq_consume.py from_existing_que -rmq 192.168.0.17 -q myqueue -c 10000
+python3 ./code/rmq_consume.py from_existing_que -rmq 95.165.29.16 -q myqueue -c 10000
 
 TIMEOUT /T -1
